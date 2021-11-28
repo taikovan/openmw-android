74dc15098ae2fd8ce7d80201b05355a7017a2e86
Sisah2 <64099446+Sisah2@users.noreply.github.com>
2021-11-28 17:48:25 +0100

    LIBGL_GAMMA -> OPENMW_GAMMA

--- app/src/main/java/ui/activity/MainActivity.kt
+++ app/src/main/java/ui/activity/MainActivity.kt
@@ -429,7 +429,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         try {
-            Os.setenv("LIBGL_GAMMA", "%.2f".format(Locale.ROOT, gamma), true)
+            Os.setenv("OPENMW_GAMMA", "%.2f".format(Locale.ROOT, gamma), true)
         } catch (e: ErrnoException) {
             // can't really do much if that fails...
         }
