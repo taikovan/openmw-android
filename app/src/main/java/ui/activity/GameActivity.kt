9379ec6235e38003b4c9ae4e1b37656416059ac2
sisah <jirielbunda@seznam.cz>
2021-11-28 11:25:40 -0500

    add back nohighp option

--- app/src/main/java/ui/activity/GameActivity.kt
+++ app/src/main/java/ui/activity/GameActivity.kt
@@ -100,10 +100,11 @@ class GameActivity : SDLActivity() {
         if (textureShrinkingOption == "high") Os.setenv("LIBGL_SHRINK", "6", true)
 
         val shaderDirOption = prefs!!.getString("pref_shadersDir_v2", "")
-        if (shaderDirOption == "modified") {
-	    Os.setenv("OPENMW_SHADERS", "modified", true)
-	    Os.setenv("LIBGL_NOHIGHP", "1", true)
-	}
+        if (shaderDirOption == "modified") Os.setenv("OPENMW_SHADERS", "modified", true)
+
+        if (PreferenceManager.getDefaultSharedPreferences(this).getBoolean("pref_nohighp", false) && shaderDirOption == "modified") {
+            Os.setenv("LIBGL_NOHIGHP", "1", true)
+        }
 
         Os.setenv("OSG_VERTEX_BUFFER_HINT", "VBO", true)
         Os.setenv("OPENMW_USER_FILE_STORAGE", Constants.USER_FILE_STORAGE + "/", true)
